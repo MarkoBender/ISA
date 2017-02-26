647090c561e6dd0a40b47dadd251069a51cfe9ea
nikolakukavica <kuka.94.delija@hotmail.com>
2017-02-26 11:27:16 +0100

    Izmenjen model regiona restorana

--- src/main/java/com/bender/Models/RestaurantRegion.java
+++ src/main/java/com/bender/Models/RestaurantRegion.java
@@ -17,15 +17,31 @@ public class RestaurantRegion {
     @Column
     private String name;
 
+    @Column
+    private double xvalue;
+
+    @Column
+    private double yvalue;
+
+    @Column
+    private double width;
+
+    @Column
+    private double height;
+
     //lazy, all
     @ManyToOne
     private Restaurant restaurant;
 
     public RestaurantRegion(){}
 
-    public RestaurantRegion(long restaurantRegion_id, String name, Restaurant restaurant) {
+    public RestaurantRegion(long restaurantRegion_id, String name, double xvalue, double yvalue, double width, double height, Restaurant restaurant) {
         this.restaurantRegion_id = restaurantRegion_id;
         this.name = name;
+        this.xvalue = xvalue;
+        this.yvalue = yvalue;
+        this.width = width;
+        this.height = height;
         this.restaurant = restaurant;
     }
 
@@ -43,6 +59,38 @@ public class RestaurantRegion {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public double getXvalue() {
+        return xvalue;
+    }
+
+    public void setXvalue(double xvalue) {
+        this.xvalue = xvalue;
+    }
+
+    public double getYvalue() {
+        return yvalue;
+    }
+
+    public void setYvalue(double yvalue) {
+        this.yvalue = yvalue;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
     }
 
     public Restaurant getRestaurant() {
