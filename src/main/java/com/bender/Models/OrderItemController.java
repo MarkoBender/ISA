fdde91e1bbb2d6cc66c0327fe8a08f7800b5ada0
nikolakukavica <kuka.94.delija@hotmail.com>
2017-02-28 14:06:23 +0100

    filtriranje ordera

--- src/main/java/com/bender/Models/OrderItemController.java
+++ src/main/java/com/bender/Models/OrderItemController.java
@@ -1,8 +1,13 @@
 package com.bender.Models;
 
+import com.bender.Beans.*;
+import com.bender.Repositories.DailyScheduleRepository;
+import com.bender.Repositories.RestaurantRegionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -13,10 +18,12 @@ import java.util.List;
 public class OrderItemController {
 
     private OrderItemRepository repository;
+    private RestaurantRegionRepository rrrepository;
 
     @Autowired
-    public OrderItemController (OrderItemRepository repository){
+    public OrderItemController (OrderItemRepository repository, RestaurantRegionRepository rrrepository){
         this.repository=repository;
+        this.rrrepository=rrrepository;
     }
 
     @RequestMapping(value = "/all" , method = RequestMethod.GET)
@@ -34,5 +41,29 @@ public class OrderItemController {
         return repository.findOne(id);
     }
 
+    @RequestMapping(value="/findOrderItemForSteward/{regionID}", method=RequestMethod.POST)
+    public List<OrderItem> getOIFS(@PathVariable long regionID, @RequestBody Steward steward){
+        List<OrderItem> ois=repository.findAll();
+        List<OrderItem> tempOIS=new ArrayList<>();
 
+        for(OrderItem oi: ois){
+            if(oi.getSteward().equals(steward) || oi.getSteward().equals(null)){
+                tempOIS.add(oi);
+            }
+        }
+
+        List<OrderItem> temp=new ArrayList<>();
+        for(OrderItem res : tempOIS){
+            Reservation reservation=res.getReservation();
+            RestaurantRegion RR=reservation.getTables().get(0).getRestaurantRegion();
+            if(RR.equals(rrrepository.findOne(regionID))) {
+                Date resDate = reservation.getDateTime();
+                resDate.setHours(resDate.getHours() + reservation.getDuration());
+                if (resDate.after(new Date()) && reservation.getDateTime().before(new Date())) {
+                    temp.add(res);
+                }
+            }
+        }
+        return temp;
+    }
 }
